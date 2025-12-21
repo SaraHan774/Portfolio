fdcc517e1753095105273b27ef74060053c352a0
Sara Han <sarahan774@gmail.com>
2025-12-21 23:44:42 +0900

    .

--- front/types/index.ts
+++ front/types/index.ts
@@ -1,3 +1,3 @@
 // Re-export all types from the new core layer for backward compatibility
 // This file maintains compatibility while we migrate imports gradually
-export * from '@/core/types';
+export * from '../src/core/types';
