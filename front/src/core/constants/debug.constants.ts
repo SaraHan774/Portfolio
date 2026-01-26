ebc84985c604bd24237f95f3887dde78a4209456
Sara Han <sarahan774@gmail.com>
2026-01-27 02:22:01 +0900

    .

--- front/src/core/constants/debug.constants.ts
+++ front/src/core/constants/debug.constants.ts
@@ -11,23 +11,25 @@
  * ```
  */
 
+const isDebug = process.env.NODE_ENV === 'development';
+
 /**
  * 디버그 레이아웃 박스 표시 여부
  * - true: 개발 환경에서 레이아웃 박스 표시
  * - false: 레이아웃 박스 숨김
  */
-export const IS_DEBUG_LAYOUT_ENABLED = true;
+export const IS_DEBUG_LAYOUT_ENABLED = isDebug;
 
 /**
  * 디버그 그리드 표시 여부
  * - true: 화면 전체에 10px 정방형 그리드 표시
  * - false: 그리드 숨김
  */
-export const IS_DEBUG_GRID_ENABLED = true;
+export const IS_DEBUG_GRID_ENABLED = isDebug;
 
 /**
  * 색상 팔레트 디버거 표시 여부
  * - true: 실시간 색상 조절 패널 표시
  * - false: 패널 숨김
  */
-export const IS_DEBUG_COLOR_ENABLED = true;
+export const IS_DEBUG_COLOR_ENABLED = isDebug;
